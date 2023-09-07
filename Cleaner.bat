7f61f806335370b422f062ccabafd29f310ae7b5
ltx0101 <126674060+ltx0101@users.noreply.github.com>
2023-09-07 17:14:13 +0300

    Cleaner.bat

--- Cleaner.bat
+++ Cleaner.bat
@@ -1,17 +1,16 @@
 @echo off
+setlocal enabledelayedexpansion
 
-timeout /t 1 /nobreak > NUL
+:: Check for administrator privileges
 openfiles > NUL 2>&1
-if %errorlevel%==0 (
-    echo Running
-) else (
+if %errorlevel% neq 0 (
     echo Please Run The Script As An Administrator!
     echo ---------------------------------------------
-    echo Right click and select 'Run as Administrator'
+    echo Right-click and select 'Run as Administrator'
     echo ---------------------------------------------
-    echo Press any key to exit..
+    echo Press any key to exit...
     pause > NUL
-    exit
+    exit /b 1
 )
 
 echo The original creator of the script is ltx0101
@@ -19,40 +18,46 @@ echo .
 echo .
 echo .
 
+:: Cleanup
 echo Cleaning Windows Temporary folder
 rmdir /s /q C:\Windows\Temp
 mkdir C:\Windows\Temp
-echo .
+echo Done
+echo.
+echo.
 
 echo Cleaning User Temporary folder
-rmdir /s /q %Temp%
-mkdir %Temp%
-echo .
+rmdir /s /q "%TEMP%"
+mkdir "%TEMP%"
+echo Done
+echo.
+echo.
 
 echo Cleaning Windows Update cache
 del /q "%windir%\SoftwareDistribution\Download\*.*"
-echo .
+echo Done
+echo.
+echo.
 
-echo Clean Windows Error Reporting files
+echo Cleaning Windows Error Reporting files
 del /q "%localappdata%\Microsoft\Windows\WER\ReportQueue\*.*"
-echo .
+echo Done
+echo.
+echo.
 
-echo Clean thumbnails cache
+echo Cleaning thumbnails cache
 del /q "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_*.db"
-echo .
+echo Done
+echo.
+echo.
 
-echo Everything is clean now!
-
-sfc /scannow
-
+:: Prompt for restart
+echo.
 echo Ready!
-
-echo Do you want to restart your PC now? (Y/N)
-set /p choice=
-if /i "%choice%"=="Y" (
+set /p restartChoice=Do you want to restart your PC now? (Y/N)
+if /i "!restartChoice!"=="Y" (
     shutdown /r /f /t 0
 ) else (
     echo Restart cancelled.
     pause
 )
-pause
