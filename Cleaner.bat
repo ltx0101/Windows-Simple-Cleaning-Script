95d2bd59cdc4997550241d8afcdec70efd3c780c
ltx0101 <126674060+ltx0101@users.noreply.github.com>
2023-10-25 14:52:20 +0300

    25/10
    Removed extra unnecessary lines and made the code faster

--- Cleaner.bat
+++ Cleaner.bat
@@ -1,59 +1,30 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:: Check for administrator privileges
-openfiles > NUL 2>&1
-if %errorlevel% neq 0 (
-    echo Please Run The Script As An Administrator!
-    echo ---------------------------------------------
-    echo Right-click and select 'Run as Administrator'
-    echo ---------------------------------------------
-    echo Press any key to exit...
-    pause > NUL
-    exit /b 1
-)
-
 echo The original creator of the script is ltx0101
 echo .
 echo .
 echo .
 
-:: Cleanup
-echo Cleaning Windows Temporary folder
-rmdir /s /q C:\Windows\Temp
-mkdir C:\Windows\Temp
-echo Done
-echo.
-echo.
+cd %temp%
+del /q /f /s *.* > nul 2>&1
+cd ..
 
-echo Cleaning User Temporary folder
-rmdir /s /q "%TEMP%"
-mkdir "%TEMP%"
-echo Done
-echo.
-echo.
+cd C:\Windows\Temp
+del /q /f /s *.* > nul 2>&1
+cd ..
 
-echo Cleaning Windows Update cache
-del /q "%windir%\SoftwareDistribution\Download\*.*"
-echo Done
-echo.
-echo.
+cd C:\Windows\Prefetch
+del /q /f /s *.* > nul 2>&1
+cd ..
 
-echo Cleaning Windows Error Reporting files
-del /q "%localappdata%\Microsoft\Windows\WER\ReportQueue\*.*"
-echo Done
-echo.
-echo.
+cd C:\Windows\SoftwareDistribution\Download
+del /q /f /s *.* > nul 2>&1
+cd ..
 
-echo Cleaning thumbnails cache
-del /q "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_*.db"
-echo Done
-echo.
-echo.
+cd %userprofile%\AppData\Local\Microsoft\Windows\Explorer\ThumbCacheToDelete
+del /q /f /s *.* > nul 2>&1
+cd ..
 
-:: Prompt for restart
-echo.
-echo Ready!
+echo Temporary files cleaned.
 set /p restartChoice=Do you want to restart your PC now? (Y/N)
 if /i "!restartChoice!"=="Y" (
     shutdown /r /f /t 0
